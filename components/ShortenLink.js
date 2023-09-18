9b9f0768832f9be2208877fe68b1a33956635c7c
jmyv08 <velarde.jm08@gmail.com>
2023-09-18 08:18:30 +0300

    Working update on ShortenLink component.

--- components/ShortenLink.js
+++ components/ShortenLink.js
@@ -1,8 +1,10 @@
 function ShortenLink() {
   return (
-    <div className="flex flex-col">
-      <input type="text" placeholder="Shorten a link here..." />
-      <button>Shorten It!</button>
+    <div className="bg-indigo-950 rounded-xl">
+      <div className="bg bg-[url('/images/bg-shorten-mobile.svg')] bg-[top_0px_right_0px] flex flex-col p-3">
+        <input className="mb-2 rounded-lg" type="text" placeholder="Shorten a link here..." />
+        <button className="bg-teal-300 rounded-lg">Shorten It!</button>
+      </div>
     </div>
   );
 }
