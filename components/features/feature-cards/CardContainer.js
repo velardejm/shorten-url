842c78495cd2dffe0981d40dccbfc5d4ade90042
jmyv08 <velarde.jm08@gmail.com>
2023-09-27 10:00:49 +0300

    Update Features component responsive styles.

--- components/features/feature-cards/CardContainer.js
+++ components/features/feature-cards/CardContainer.js
@@ -1,16 +1,30 @@
 import CardContent from "./CardContent";
 import CardIcon from "./CardIcon";
 
-import { getStyle } from '@/utils/utilities';
+import styles from "@/styles/CardContainer.module.css";
 
 export default function CardContainer({ feature, isLastItem, index }) {
 
+  function setMarginTop() {
+    switch (index) {
+      case 1:
+        return styles.mdMarginTop1;
+      case 2:
+        return styles.mdMarginTop2;
+      case 3:
+        return styles.mdMarginTop3;
+      default:
+        break;
+    }
+  }
+
+
   return (
     <>
-      <div className={`bg-white pb-10 h-min ${getStyle(index)}` }>
+      <div className={`bg-white pb-10 h-min ${setMarginTop()}`}>
         <CardIcon iconUrl={feature.iconUrl} />
         <CardContent feature={feature} />
-      </div>
+      </div >
 
       <div
         className={`w-2 h-20 bg-teal-300 mx-auto md:w-20 md:h-2 md:self-center ${isLastItem ? "hidden" : "block"}`}
